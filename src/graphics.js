4bca072cc8a8da767e3e335aed8a01abec8c1b71
gustavo domingues <deathdiec@gmail.com>
2022-09-16 10:02:45 -0300

    added vscode buttons

--- src/graphics.js
+++ src/graphics.js
@@ -3,6 +3,6 @@ import test from './import.js';
 export default function helloWorld(){
     console.log('hello world!');
     var t = document.getElementById('test');
-    t.innerHTML = "HELLsssO WOssRLD!";
+    t.innerHTML = "HELLO WORLD!";
     test();
 }
