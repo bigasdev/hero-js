8d6c273ffe1ede596d55307780c9e27062ca51aa
gustavo domingues <deathdiec@gmail.com>
2022-09-16 08:54:21 -0300

    yup it works.

--- src/graphics.js
+++ src/graphics.js
@@ -2,5 +2,7 @@ import test from './import.js';
 
 export default function helloWorld(){
     console.log('hello world!');
+    var t = document.getElementById('test');
+    t.innerHTML = "HELLO WORLD!";
     test();
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ['Test again'], ' ');
     helloWorld();
   
     return element;
