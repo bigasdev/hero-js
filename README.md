214224f3526fa7663078dcbeb4d447d7cc3b49bf
bigasdev <61640594+bigasdev@users.noreply.github.com>
2022-09-23 14:42:14 +0000

    Build: (358384b) finished the structure + first test layout

--- README.md
+++
@@ -1,2 +0,0 @@
-# bigas-website-learning
- 
--- src/graphics.js
+++
@@ -1,8 +0,0 @@
-import test from './import.js';
-
-export default function helloWorld(){
-    console.log('hello world!');
-    var t = document.getElementById('test');
-    t.innerHTML = "HELLO WORLD!";
-    test();
-}
