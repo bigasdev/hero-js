9c17125d4f464148ae917f37c19656eb10060fd1
gustavo domingues <deathdiec@gmail.com>
2022-09-23 14:51:40 -0300

    favicon working i think?

--- src/graphics.js
+++ src/graphics.js
@@ -1,3 +1,4 @@
+import { Asset, createAsset } from "./assets";
 import { printText } from "./font";
 import { getCanvas, getContext, setCanvas } from "./global";
 import { getMiddleX, getMiddleY } from "./util";
@@ -7,7 +8,8 @@ export default function start(){
     //setting the canvases;
     setCanvas(0, document.getElementById('canvas'));
 
+    createAsset(new Asset("logo", getMiddleX(getCanvas(0), -45), getMiddleY(getCanvas(0), -140), 3), getContext(0));
     printText("Hello World!!", getMiddleX(getCanvas(0), -70), getMiddleY(getCanvas(0), -50), 2, 0);
-    printText("This website was powered by the bigasdev framework.", getMiddleX(getCanvas(0), -140), getMiddleY(getCanvas(0), -20), 1, 0);
+    printText("This website was powered by the hero framework.", getMiddleX(getCanvas(0), -140), getMiddleY(getCanvas(0), -20), 1, 0);
 
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import fontAtlas from '/assets/img/fontAtlas.png';
+import atlas from '/assets/img/atlas.png';
 import fontJson from '/assets/atlas-sources/font.json';
+import atlasJson from '/assets/atlas-sources/atlas.json';
 
 import _ from 'lodash';
 import { asepriteConfig } from './assets.js';
@@ -11,7 +13,7 @@ function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    asepriteConfig([fontAtlas], [fontJson]);
+    asepriteConfig([fontAtlas, atlas], [fontJson, atlasJson]);
 
     //quick delay so we have time to load everything.
     setTimeout(()=>{
