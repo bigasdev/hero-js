d3b93c09d96f58f901b3353c7147d7d9c11d1f51
gustavo domingues <deathdiec@gmail.com>
2022-09-23 15:10:32 -0300

    trying to fix again

--- src/graphics.js
+++ src/graphics.js
@@ -9,8 +9,9 @@ export default function start(){
     console.log("started the graphics.js");
     setCanvas(0, document.getElementById('canvas'));
 
-    createAsset(new Asset("logo", getMiddleX(getCanvas(0), -45), getMiddleY(getCanvas(0), -140), 3), getContext(0));
-    printText("Hello World!!", getMiddleX(getCanvas(0), -70), getMiddleY(getCanvas(0), -50), 2, 0);
-    printText("This website was powered by the hero framework.", getMiddleX(getCanvas(0), -140), getMiddleY(getCanvas(0), -20), 1, 0);
-
+    setTimeout(() => {
+        createAsset(new Asset("logo", getMiddleX(getCanvas(0), -45), getMiddleY(getCanvas(0), -140), 3), getContext(0));
+        printText("Hello World!!", getMiddleX(getCanvas(0), -70), getMiddleY(getCanvas(0), -50), 2, 0);
+        printText("This website was powered by the hero framework.", getMiddleX(getCanvas(0), -140), getMiddleY(getCanvas(0), -20), 1, 0);
+    }, 200);
 }
